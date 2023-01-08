4796b6b26086cb7cfb20e4e3520880e4fc52f046
Adam Brown <adampsbrown@gmail.com>
2023-01-08 12:18:48 +0000

    syncinglatest version

--- sync.sh
+++ sync.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-# TAG=$1
-
-TAG="07/11/2022-V1"
+TAG=$(curl https://api.github.com/repos/ouchadam/small-talk/releases/latest | jq ".tag_name")
 NORMALISED_TAG=$(echo $TAG | tr '/' '.')
 UPSTREAM_FILENAME=$(echo "foss-signed-$TAG.apk" | tr '/' '.')
 URL_ENCODED_TAG=$(echo $TAG | sed -e "s/\//%2F/g")
 
-wget "https://github.com/ouchadam/small-talk/releases/download/$URL_ENCODED_TAG/$UPSTREAM_FILENAME"
+wget "https://github.com/ouchadam/small-talk/releases/latest/download/$UPSTREAM_FILENAME"
 mkdir repo
 mv $UPSTREAM_FILENAME "repo/small-talk-$NORMALISED_TAG.apk"
 
