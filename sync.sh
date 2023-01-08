c10f0297152c6b6d408424f3875fd6ee5cc9e76c
Adam Brown <adampsbrown@gmail.com>
2023-01-08 11:55:49 +0000

    extra steps

--- sync.sh
+++ sync.sh
@@ -8,6 +8,12 @@ UPSTREAM_FILENAME=$(echo "foss-signed-$TAG.apk" | tr '/' '.')
 URL_ENCODED_TAG=$(echo $TAG | sed -e "s/\//%2F/g")
 
 wget "https://github.com/ouchadam/small-talk/releases/download/$URL_ENCODED_TAG/$UPSTREAM_FILENAME"
+mkdir repo
 mv $UPSTREAM_FILENAME "repo/small-talk-$NORMALISED_TAG.apk"
 
 fdroid update
+
+git checkout repo
+git add repo
+git commit -am "Syncing repo"
+git push
