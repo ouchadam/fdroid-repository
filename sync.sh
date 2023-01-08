8d046bf756b016021221b6a1da2340332df29868
Adam Brown <adampsbrown@gmail.com>
2023-01-08 11:52:40 +0000

    include app name in apk file

--- sync.sh
+++ sync.sh
@@ -8,6 +8,6 @@ UPSTREAM_FILENAME=$(echo "foss-signed-$TAG.apk" | tr '/' '.')
 URL_ENCODED_TAG=$(echo $TAG | sed -e "s/\//%2F/g")
 
 wget "https://github.com/ouchadam/small-talk/releases/download/$URL_ENCODED_TAG/$UPSTREAM_FILENAME"
-mv $UPSTREAM_FILENAME "repo/$NORMALISED_TAG.apk"
+mv $UPSTREAM_FILENAME "repo/small-talk-$NORMALISED_TAG.apk"
 
 fdroid update
