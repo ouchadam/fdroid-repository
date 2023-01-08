e145672da4e53a46454a84418ce094919e76f6a8
Adam Brown <adampsbrown@gmail.com>
2023-01-08 12:20:11 +0000

    Syncing repo

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-TAG=$(curl https://api.github.com/repos/ouchadam/small-talk/releases/latest | jq ".tag_name")
+TAG=$(curl https://api.github.com/repos/ouchadam/small-talk/releases/latest | jq -r ".tag_name")
 NORMALISED_TAG=$(echo $TAG | tr '/' '.')
 UPSTREAM_FILENAME=$(echo "foss-signed-$TAG.apk" | tr '/' '.')
 URL_ENCODED_TAG=$(echo $TAG | sed -e "s/\//%2F/g")
